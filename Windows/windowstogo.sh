69fa51d97dd33d4fbd70bb151f9d7d947b4523a2
jpz4085 <jpz408@yahoo.com>
2024-08-11 18:43:18 -0400

    Add GPLv3 license.

--- Windows/windowstogo.sh
+++ Windows/windowstogo.sh
@@ -5,6 +5,20 @@
 #  Based on the WTG PowerShell procedure at the link below.
 #  https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/deployment/windows-to-go/deploy-windows-to-go
 #
+# Copyright (C) 2024 Joseph P. Zeller
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 system="$1"
 wimfile="$2"
